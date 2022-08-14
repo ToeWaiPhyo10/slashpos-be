c5eeb1c5798b30f7664c9ab4a4a285d72e75a232
ToeWaiPhyo <toewai@nexbe.sg>
2022-08-14 12:22:10 +0630

    deployment

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
-const path = require('path');
-
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'slashpos'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'root'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env.bool("DATABASE_SSL", false),
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
