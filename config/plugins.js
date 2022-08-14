e0f06f305693c344155ffe55dfddaefb79c37716
ToeWaiPhyo <toewai@nexbe.sg>
2022-08-14 12:58:49 +0630

    add graphql

--- config/plugins.js
+++ config/plugins.js
@@ -2,6 +2,17 @@ module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
       jwtSecret: env('JWT_SECRET'),
-    }
+    },
+    graphql: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 7,
+      amountLimit: 100,
+      introspection: true,
+      apolloServer: {
+        tracing: false,
+      },
+    },
   }
 })
